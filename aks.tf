fad603db9e6813601e8e0f945ca4689c688e6a95
sean.g <sean.gnanarajah@gmail.com>
2024-09-10 23:51:11 -0400

    delete aks

--- aks.tf
+++ aks.tf
@@ -1,38 +1,3 @@
-#Provision AKS cluster in Azure
-
-resource "azurerm_kubernetes_cluster" "vuln_k8_cluster" {
-  name                = "${var.victim_company}-kubecluster"
-  location            = azurerm_resource_group.victim-network-rg.location
-  resource_group_name = azurerm_resource_group.victim-network-rg.name
-  dns_prefix          = "${var.victim_company}-k8"
-
-  default_node_pool {
-    name       = "default"
-    node_count = var.nodecount
-    vm_size    = "Standard_D2_v2"
-  }
-
- service_principal {
-    client_id     = var.client_id
-    client_secret = var.client_secret
-  }
-
-  network_profile {
-    network_plugin     = "azure"
-    network_policy     = "calico"     # Options are calico or azure - only if network plugin is set to azure
-    dns_service_ip     = "172.16.0.10" # Required when network plugin is set to azure, must be in the range of service_cidr and above 1
-    docker_bridge_cidr = "172.17.0.1/16"
-    service_cidr       = "172.16.0.0/16" # Must not overlap any address from the VNEt
-  }
-
-  lifecycle {
-    ignore_changes = [
-      windows_profile,
-    ]
- }
-}
-
-###Adding stuff
 
 #Authenticate to Terraform Kubernetes Module
 
